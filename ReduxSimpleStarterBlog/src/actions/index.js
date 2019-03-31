a1976e7c2dc36231d4e361f859046eb20378a2ec
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-03-31 16:18:04 -0300

    Update index.js

--- ReduxSimpleStarterBlog/src/actions/index.js
+++ ReduxSimpleStarterBlog/src/actions/index.js
@@ -6,7 +6,7 @@ export const FETCH_POST = 'FETCH_POST';
 export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=pedromdev';
+const API_KEY = '?key=';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
