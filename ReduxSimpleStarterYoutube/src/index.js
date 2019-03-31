f0a4e015e0911553c3be57140d9ef649c183708d
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-03-31 16:17:11 -0300

    Update index.js

--- ReduxSimpleStarterYoutube/src/index.js
+++ ReduxSimpleStarterYoutube/src/index.js
@@ -6,7 +6,7 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyB0pFr_u7v6L4gDIVypqT_oCF-o3vAqO5U';
+const API_KEY = '';
 
 class App extends Component {
   constructor(props) {
