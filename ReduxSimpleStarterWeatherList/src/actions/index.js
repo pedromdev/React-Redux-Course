aa6b279a92f7c1a20c9655e0c19912903b2d8e3a
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-03-31 16:17:26 -0300

    Update index.js

--- ReduxSimpleStarterWeatherList/src/actions/index.js
+++ ReduxSimpleStarterWeatherList/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '541ecca38949c57d796a51c2c0e9af36';
+const API_KEY = '';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
